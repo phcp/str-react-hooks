9d5b725501586a31a9d5efd0521c3a3c4f7c34b0
Alany Maria <lamariasilva@MacBook-Pro-de-Alany.local>
2020-01-03 13:59:41 -0300

    Changes url to get posts

--- client/src/feed/Feed.js
+++ client/src/feed/Feed.js
@@ -11,7 +11,7 @@ export default class Feed extends React.Component {
     }
 
     componentDidMount() {
-        fetch('http://5dd6ba29.ngrok.io/posts')
+        fetch('http://7380c9a6.ngrok.io/users/1/posts')
             .then(posts => posts.json())
             .then(posts => {
                 this.setState({posts})
