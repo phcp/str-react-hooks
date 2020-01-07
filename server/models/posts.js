fbe6b1c30bf28ba2ae37e0474a8f77ab2251a57d
Paulo Henrique da Cruz Pereira <lapaulo.cruz@Paulos-MacBook-Pro.local>
2020-01-07 11:25:10 -0300

    Update posts

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,42 +1,42 @@
 const posts = [{
     userId: '1',
-    description: "Description 1",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    description: " His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. 'What's happened to me?' he thought.",
+    imageUrl: "https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg",
 },
 {
     userId: '1',
-    description: "Description 2",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    description: "The bedding was hardly able to cover it and seemed ready to slide off any moment.",
+    imageUrl: "https://occ-0-1007-1009.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABceL_FxRrEg1Jm2LyiYugCJwBkJ2v4GmCBWQ_JNLBXCu1tpO1CMoOxGk9R74PCzrCR0FLIrjdgZlyIHnZEuiHArY6C9G.jpg?r=a82",
 },
 {
     userId: '1',
-    description: "Description 3",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    description: "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.",
+    imageUrl: "https://images.unsplash.com/photo-1517021897933-0e0319cfbc28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
 },
 {
     userId: '2',
     description: "Description 4",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://media.nature.com/w700/magazine-assets/d41586-019-02643-z/d41586-019-02643-z_17128648.jpg",
 },
 {
     userId: '2',
     description: "Description 5",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://uiaa-web.azureedge.net/wp-content/uploads/2017/12/2018_banner.jpg",
 },
 {
     userId: '2',
     description: "Description 6",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://cdn.pixabay.com/photo/2019/09/12/13/29/sunset-4471603_960_720.jpg",
 },
 {
     userId: '3',
     description: "Description 7",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://i.ytimg.com/vi/cQ5Fi29BbTE/maxresdefault.jpg",
 },
 {
     userId: '3',
     description: "Description 8",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://i.pinimg.com/originals/41/29/11/4129111b850a1aaf6a26635aeb24234c.jpg",
 }];
 
 module.exports = posts;
