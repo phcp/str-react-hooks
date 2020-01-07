6b214965641e8d481eba5d624b9ea5e0f3a7cc1c
Paulo Henrique da Cruz Pereira <lapaulo.cruz@Paulos-MacBook-Pro.local>
2020-01-07 11:30:19 -0300

    Update user images

--- server/models/users.js
+++ server/models/users.js
@@ -1,22 +1,22 @@
 const users = [{
     id: '1',
     name: "Alany Maria",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://i.pinimg.com/236x/16/42/02/1642028a02a31367ac695a65024b2592--hd-wallpaper-iphone-wallpaper-disney.jpg",
 },
 {
     id: '2',
     name: "Paulo Cruz",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://i.pinimg.com/236x/93/56/ff/9356ffeeadd9faf086c1432aa7625e8b--penguin-tattoo-madagaskar.jpg",
 },
 {
     id: '3',
     name: "Ruan Silva",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://http2.mlstatic.com/boneco-de-pelucia-pinguim-madagascar-presente-pronta-entrega-D_NQ_NP_878898-MLB31831827118_082019-F.jpg",
 },
 {
     id: '4',
     name: "Emerson Silva",
-    imageUrl: "https://www.pedegoelectricbikes.com/wp-content/uploads/2018/08/pedego-trail-tracker-5.jpg",
+    imageUrl: "https://live.staticflickr.com/5102/5561461443_9bcd8b3b73_c.jpg",
 }];
 
 module.exports = users;
