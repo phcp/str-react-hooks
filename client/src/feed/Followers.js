04f6f7a39087e6cba28dca3f9901cc16ce096e5a
Ruan Victor <ruan.techno@gmail.com>
2020-01-07 18:01:39 -0300

    Adicionando hooks

---
+++ client/src/feed/Followers.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Post from './Post';
+import { useFetch } from '../hooks/useFetch'
+
+const Followers = () => {
+    const users = useFetch('/users');
+
+    return (
+        users.map((user, index) => (
+            <Post
+                key={index}
+                description={user.name}
+                imageUrl={user.imageUrl}
+            />
+        ))
+    );
+}
+
+export default Followers;
